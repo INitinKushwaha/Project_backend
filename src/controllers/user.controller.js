3e26f0a6f1ec7c8fda6e686a7a573ca58f74fa3c
Nitin Kushwaha <72150554+gigglernitin@users.noreply.github.com>
2024-06-21 00:46:03 +0530

    chore: Refactor user registration controller and fix file upload bug

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -22,7 +22,8 @@ const registerUser = asyncHandler( async (req,res) =>{
 //    console.log("email: ", email);
    
     if (
-        [fullName, email, username, password].some((field) => field?.trim() === "")
+        [fullName, email, username, password].some((field) => 
+            field?.trim() === "")
     ) {
         throw new ApiError(400, "All fields are required")
     }
@@ -43,7 +44,14 @@ const registerUser = asyncHandler( async (req,res) =>{
 
 
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    // sconst coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    let coverImageLocalPath ;
+    if(req.files && Array.isArray(req.files.coverImage)&& req.files.coverImage.length > 0){
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
+
+
 
     if (!avatarLocalPath) {
         throw new ApiError(400, "Avatar file is required")
@@ -52,13 +60,13 @@ const registerUser = asyncHandler( async (req,res) =>{
     const avatar = await uploadOnCloudinary(avatarLocalPath)
     const coverImage = await uploadOnCloudinary(coverImageLocalPath)
 
-    if (!avatar) {
-        throw new ApiError(400, "Avatar file is local path required")
-    }
+    // if (!avatar) {
+    //     throw new ApiError(400, "Avatar is not upload on cloundinary")
+    // }
 
     const user = await User.create({
         fullName,
-        avatar: avatar.url,
+        avatar: avatar?.url || "",
         coverImage: coverImage?.url || "",
         email, 
         password,
