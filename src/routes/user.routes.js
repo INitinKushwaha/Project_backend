344750ce9f1fd3df46af165df04300ea694b463a
Nitin Kushwaha <72150554+gigglernitin@users.noreply.github.com>
2024-06-22 02:50:04 +0530

    chore: Add getUserChannelProfile and getWatchHistory endpoints, refactor user controller, and update user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,10 @@
 import {Router} from 'express';
-import {loginUser, logoutUser, registerUser, refreshAccessToken} from '../controllers/user.controller.js';
+import {loginUser, logoutUser,
+     registerUser, refreshAccessToken, 
+     changeCurrentPassword, getCurrentUser,
+      updateAccountDetails, updateUserAvatar,
+       updateUserCoverImage, getUserChannelProfile,
+        getWatchHistory} from '../controllers/user.controller.js';
 import {upload} from "../middlerwares/multer.middlerware.js"
 import {verifyJWT} from "../middlerwares/auth.middleware.js"
 
@@ -24,6 +29,18 @@ router.route("/login").post(loginUser)
 
 // secured routes
 router.route("/logout").post(verifyJWT  ,logoutUser)
-route.route("/refresh-route").post(refreshAccessToken)
+router.route("/refresh-route").post(refreshAccessToken)
+
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/cuurent-user").get(verifyJWT, getCurrentUser)
+
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
+router.route("/C/:username").get(verifyJWT, getUserChannelProfile)
+
+router.route("/watchHistory").get(verifyJWT, getWatchHistory)
+
 
 export default router;
